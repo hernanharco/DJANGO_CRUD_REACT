983e5f60080607efbd152881402f0b8e271d7bbd
hernanharco <hernan.harco@gmail.com>
2025-03-28 12:32:39 +0100

    v1.1

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,6 +1,24 @@
 
 import axios from 'axios'
 
-export const getAllTasks = () => {
-    return axios.get('http://localhost:8000/tasks/api/v1/tasks/')
+const tasksApi = axios.create({
+    //En esta parte tuvimos un error ya que se debe escribir bien el baseURL tener encuenta las mayusculas
+    baseURL: "http://localhost:8000/tasks/api/v1/tasks/"
+})
+
+//Consultar
+export const getAllTasks = () => tasksApi.get('/')
+
+//Consultar por id
+export const getTask = (id) => tasksApi.get(`/${id}/`)
+
+//Agregar
+export const createTask = (task) => {
+    return tasksApi.post('/', task)
 }
+
+//Eliminar
+export const deleteTask = (id) => tasksApi.delete(`/${id}/`)
+
+//Actualizar
+export const updateTask = (id, task) => tasksApi.put(`/${id}/`, task)
--- client/src/components/TaskList.jsx
+++ client/src/components/TaskList.jsx
@@ -1,19 +1,29 @@
 
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { getAllTasks } from "../api/tasks.api";
+import { TaskCard } from "./TaskCard";
 
 export function TaskList() {
+    const [tasks, setTasks] = useState([]);
 
-    useEffect(() => { 
-
+    useEffect(() => {
         async function loadTasks() {
             const res = await getAllTasks();
-            console.log(res);
+            //console.log(res);
+            setTasks(res.data);
         }
+
         loadTasks();
+
     }, []);
 
-    return <div>TaskList</div>
-    
+    return (
+        <div className="grid grid-cols-3 gap-3">
+            {tasks.map((task) => (
+                <TaskCard key={task.id} task={task} />
+            ))}
+        </div>
+    )
+
 }
 
